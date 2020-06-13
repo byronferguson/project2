cfc83edae54e4cbe1c8ccddd4198866cb6428401
Byron Ferguson <ferguson.bs@gmail.com>
2020-06-13 01:36:28 -0500

    adding example migration

---
+++ migrations/20191212055649-create-example.js
@@ -0,0 +1,30 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("examples", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      test: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, _Sequelize) => {
+    return queryInterface.dropTable("examples");
+  }
+};
